6e24b850909b8d9a7f185eff62ec6261b3d751d1
Qiqiuuu <160641972+Qiqiuuu@users.noreply.github.com>
2025-07-06 01:48:00 +0200

    migrations

---
+++ TarkovBot/Migrations/20250705135626_AddTwitterMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TarkovBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTwitterMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TwitterPosts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChannelId = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    AccountId = table.Column<long>(type: "bigint", nullable: false),
+                    LastTweetId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TwitterPosts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TwitterPosts");
+        }
+    }
+}
